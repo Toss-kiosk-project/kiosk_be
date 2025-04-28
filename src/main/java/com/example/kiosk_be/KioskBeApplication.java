1ff94f9363d07f9260bade324e9558292dd2a311
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-04-28 14:13:14 +0900

    Initial Setting

---
+++ src/main/java/com/example/kiosk_be/KioskBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiosk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiosk_be/KioskBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiosk_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
